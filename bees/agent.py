d9c928841d5eb8b4f759705213a0c1ab240db1c8
brendanxwhitaker <whitaker.213@osu.edu>
2019-08-11 12:04:31 -0400

    Added move and consume functions.

--- bees/agent.py
+++ bees/agent.py
@@ -1,10 +1,11 @@
+from typing import Tuple
+
 class Agent:
 
-    def __init__(self, pos=None):
+    def __init__(self, pos: Tuple[int] = None, health: float = 1) -> None:
+        """``health`` ranges in ``[0, 1]``."""
         self.pos = pos
+        self.health = health
     
     def reset(self):
-        pass
-    
-    def step(self):
-        pass
+        pass 
