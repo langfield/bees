563062184f6e464f2138453423dd0b34d5f64117
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2019-08-20 10:02:38 -0400

    Trying to fix observation space mismatch, not completely fixed yet

--- bees/utils.py
+++ bees/utils.py
@@ -6,3 +6,23 @@ def one_hot(k: int, n: int) -> np.ndarray:
     vec = np.zeros([n])
     vec[k] = 1
     return vec
+
+def convert_obs_to_tuple(obs: np.ndarray):
+
+    outerList = []
+    for x in range(obs.shape[0]):
+
+        innerList = []
+        for y in range(obs.shape[1]):
+
+            # Convert floats to int
+            pointObs = obs[x, y]
+            for c in range(pointObs.shape[0]):
+                pointObs[c] = int(pointObs[c])
+
+            innerList.append(tuple(pointObs))
+
+        outerList.append(tuple(innerList))
+
+    return tuple(outerList)
+
