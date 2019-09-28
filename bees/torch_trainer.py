1c27c1776c76d2a986fe1574302a4915ee8c5dee
Brendan Whitaker <whitaker.213@osu.edu>
2019-09-28 18:43:22 +0000

    Finished all but stats.

--- bees/torch_trainer.py
+++ bees/torch_trainer.py
@@ -38,19 +38,29 @@ def main():
     torch.set_num_threads(1)
     device = torch.device("cuda:0" if args.cuda else "cpu")
 
-    env = make_vec_envs(args.env_name, args.seed, args.num_processes,
-                         args.gamma, args.log_dir, device, False)
+    env = make_vec_envs(
+        args.env_name,
+        args.seed,
+        args.num_processes,
+        args.gamma,
+        args.log_dir,
+        device,
+        False,
+    )
 
-
-    def get_agent(args: args.Namespace, obs_space: gym.space, act_space: gym.space, device: torch.device) -> Tuple["AgentAlgo", Policy, RolloutStorage]:
+    def get_agent(
+        args: args.Namespace,
+        obs_space: gym.space,
+        act_space: gym.space,
+        device: torch.device,
+    ) -> Tuple["AgentAlgo", Policy, RolloutStorage]:
 
         actor_critic = Policy(
-            obs_space.shape,
-            act_space,
-            base_kwargs={'recurrent': args.recurrent_policy})
+            obs_space.shape, act_space, base_kwargs={"recurrent": args.recurrent_policy}
+        )
         actor_critic.to(device)
 
-        if args.algo == 'a2c':
+        if args.algo == "a2c":
             agent = algo.A2C_ACKTR(
                 actor_critic,
                 args.value_loss_coef,
@@ -58,8 +68,9 @@ def main():
                 lr=args.lr,
                 eps=args.eps,
                 alpha=args.alpha,
-                max_grad_norm=args.max_grad_norm)
-        elif args.algo == 'ppo':
+                max_grad_norm=args.max_grad_norm,
+            )
+        elif args.algo == "ppo":
             agent = algo.PPO(
                 actor_critic,
                 args.clip_param,
@@ -69,27 +80,36 @@ def main():
                 args.entropy_coef,
                 lr=args.lr,
                 eps=args.eps,
-                max_grad_norm=args.max_grad_norm)
-        elif args.algo == 'acktr':
+                max_grad_norm=args.max_grad_norm,
+            )
+        elif args.algo == "acktr":
             agent = algo.A2C_ACKTR(
-                actor_critic, args.value_loss_coef, args.entropy_coef, acktr=True)
+                actor_critic, args.value_loss_coef, args.entropy_coef, acktr=True
+            )
 
-        rollouts = RolloutStorage(args.num_steps, args.num_processes,
-                                  obs_space.shape, act_space,
-                                  actor_critic.recurrent_hidden_state_size)
+        rollouts = RolloutStorage(
+            args.num_steps,
+            args.num_processes,
+            obs_space.shape,
+            act_space,
+            actor_critic.recurrent_hidden_state_size,
+        )
 
     # Create multiagent maps.
     actor_critics: Dict[str, Policy] = {}
     agents: Dict[str, "AgentAlgo"] = {}
     rollout_map: Dict[str, RolloutStorage] = {}
     episode_rewards: Dict[str, collections.deque] = {}
-    
+    minted_agents: Set[str] = set()
+
     obs = env.reset()
 
     # Initialize first policies.
     for agent_id, agent_obs in obs.items():
         if agent_id not in agents:
-            agent, actor_critic, rollouts = get_agent(args, env.observation_space, env.action_space, device)
+            agent, actor_critic, rollouts = get_agent(
+                args, env.observation_space, env.action_space, device
+            )
             agents[agent_id] = agent
             actor_critics[agent_id] = actor_critic
             rollout_map[agent_id] = rollouts
@@ -101,30 +121,40 @@ def main():
         rollouts.to(device)
 
     start = time.time()
-    num_updates = int(
-        args.num_env_steps) // args.num_steps // args.num_processes
+    num_updates = int(args.num_env_steps) // args.num_steps // args.num_processes
     for j in range(num_updates):
 
         if args.use_linear_lr_decay:
             # decrease learning rate linearly
             utils.update_linear_schedule(
-                agent.optimizer, j, num_updates,
-                agent.optimizer.lr if args.algo == "acktr" else args.lr)
+                agent.optimizer,
+                j,
+                num_updates,
+                agent.optimizer.lr if args.algo == "acktr" else args.lr,
+            )
 
         for step in range(args.num_steps):
 
-            actor_critic_return_dict: Dict[str, Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]] = {}
+            minted_agents = set()
+
+            actor_critic_return_dict: Dict[
+                str, Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]
+            ] = {}
 
             # Sample actions
             with torch.no_grad():
                 for agent_id, actor_critic in actor_critics.items():
                     rollouts = rollout_map[agent_id]
                     value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
-                        rollouts.obs[step], rollouts.recurrent_hidden_states[step],
-                        rollouts.masks[step])
+                        rollouts.obs[step],
+                        rollouts.recurrent_hidden_states[step],
+                        rollouts.masks[step],
+                    )
                     actor_critic_return_dict[agent_id] = actor_critic.act(
-                        rollouts.obs[step], rollouts.recurrent_hidden_states[step],
-                        rollouts.masks[step])
+                        rollouts.obs[step],
+                        rollouts.recurrent_hidden_states[step],
+                        rollouts.masks[step],
+                    )
 
             # Obser reward and next obs
             obs, rewards, dones, infos = env.step(action)
@@ -132,36 +162,38 @@ def main():
             # NOTE: we assume ``args.num_processes`` is ``1``.
 
             for agent_id in obs.keys():
-                
+                agent_obs = obs[agent_id]
+                agent_reward = rewards[agent_id]
+                agent_done = dones[agent_id]
+                agent_info = infos[agent_id]
+
                 # Initialize new policies.
                 if agent_id not in agents:
-                    agent, actor_critic, rollouts = get_agent(args, env.observation_space, env.action_space, device)
-                    
+                    minted_agents.add(agent_id)
+                    agent, actor_critic, rollouts = get_agent(
+                        args, env.observation_space, env.action_space, device
+                    )
+
                     # Copy first observations to rollouts, and send to device.
                     rollouts = rollout_map[agent_id]
                     rollouts.obs[0].copy_(agent_obs)
                     rollouts.to(device)
-                   
-                    # Update dicts. 
+
+                    # Update dicts.
                     agents[agent_id] = agent
                     actor_critics[agent_id] = actor_critic
                     rollout_map[agent_id] = rollouts
 
                 else:
-                    agent_obs = obs[agent_id]
-                    agent_reward = rewards[agent_id]
-                    agent_done = dones[agent_id]
-                    agent_info = infos[agent_id]
-                    
-                    if 'episode' in agent_info.keys():
-                        episode_rewards[agent_id].append(agent_info['episode']['r'])
+                    if "episode" in agent_info.keys():
+                        episode_rewards[agent_id].append(agent_info["episode"]["r"])
 
                     # If done then clean the history of observations.
                     if agent_done:
                         masks = torch.FloatTensor([[0.0]])
                     else:
                         masks = torch.FloatTensor([[1.0]])
-                    if 'bad_transition' in agent_info.keys():
+                    if "bad_transition" in agent_info.keys():
                         bad_masks = torch.FloatTensor([[0.0]])
                     else:
                         bad_masks = torch.FloatTensor([[1.0]])
@@ -172,53 +204,94 @@ def main():
 
                     # Add to rollouts.
                     value_tuple = actor_critic_return_dict[agent_id]
-                    value, action, action_log_prob, recurrent_hidden_states = value_tuple
-                    rollout_map[agent_id].insert(obs_tensor, recurrent_hidden_states, action,
-                                    action_log_prob, value, reward_array, masks, bad_masks)
+                    value, action, action_log_prob, recurrent_hidden_states = (
+                        value_tuple
+                    )
+                    rollout_map[agent_id].insert(
+                        obs_tensor,
+                        recurrent_hidden_states,
+                        action,
+                        action_log_prob,
+                        value,
+                        reward_array,
+                        masks,
+                        bad_masks,
+                    )
 
-        with torch.no_grad():
-            next_value = actor_critic.get_value(
-                rollouts.obs[-1], rollouts.recurrent_hidden_states[-1],
-                rollouts.masks[-1]).detach()
+        for agent_id, agent in agents.items():
+            if agent_id not in minted_agents:
 
-        rollouts.compute_returns(next_value, args.use_gae, args.gamma,
-                                 args.gae_lambda, args.use_proper_time_limits)
+                actor_critic = actor_critics[agent_id]
+                rollouts = rollout_map[agent_id]
 
-        value_loss, action_loss, dist_entropy = agent.update(rollouts)
+                with torch.no_grad():
+                    next_value = actor_critic.get_value(
+                        rollouts.obs[-1],
+                        rollouts.recurrent_hidden_states[-1],
+                        rollouts.masks[-1],
+                    ).detach()
 
-        rollouts.after_update()
+                rollouts.compute_returns(
+                    next_value,
+                    args.use_gae,
+                    args.gamma,
+                    args.gae_lambda,
+                    args.use_proper_time_limits,
+                )
+
+                value_loss, action_loss, dist_entropy = agent.update(rollouts)
+
+                rollouts.after_update()
 
         # save for every interval-th episode or for the last epoch
-        if (j % args.save_interval == 0
-                or j == num_updates - 1) and args.save_dir != "":
+        if (
+            j % args.save_interval == 0 or j == num_updates - 1
+        ) and args.save_dir != "":
             save_path = os.path.join(args.save_dir, args.algo)
             try:
                 os.makedirs(save_path)
             except OSError:
                 pass
 
-            torch.save([
-                actor_critic,
-                getattr(utils.get_vec_normalize(env), 'ob_rms', None)
-            ], os.path.join(save_path, args.env_name + ".pt"))
+            torch.save(
+                [actor_critic, getattr(utils.get_vec_normalize(env), "ob_rms", None)],
+                os.path.join(save_path, args.env_name + ".pt"),
+            )
 
         if j % args.log_interval == 0 and len(episode_rewards) > 1:
             total_num_steps = (j + 1) * args.num_processes * args.num_steps
             end = time.time()
             print(
-                "Updates {}, num timesteps {}, FPS {} \n Last {} training episodes: mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}\n"
-                .format(j, total_num_steps,
-                        int(total_num_steps / (end - start)),
-                        len(episode_rewards), np.mean(episode_rewards),
-                        np.median(episode_rewards), np.min(episode_rewards),
-                        np.max(episode_rewards), dist_entropy, value_loss,
-                        action_loss))
+                "Updates {}, num timesteps {}, FPS {} \n Last {} training episodes: mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}\n".format(
+                    j,
+                    total_num_steps,
+                    int(total_num_steps / (end - start)),
+                    len(episode_rewards),
+                    np.mean(episode_rewards),
+                    np.median(episode_rewards),
+                    np.min(episode_rewards),
+                    np.max(episode_rewards),
+                    dist_entropy,
+                    value_loss,
+                    action_loss,
+                )
+            )
 
-        if (args.eval_interval is not None and len(episode_rewards) > 1
-                and j % args.eval_interval == 0):
+        if (
+            args.eval_interval is not None
+            and len(episode_rewards) > 1
+            and j % args.eval_interval == 0
+        ):
             ob_rms = utils.get_vec_normalize(env).ob_rms
-            evaluate(actor_critic, ob_rms, args.env_name, args.seed,
-                     args.num_processes, eval_log_dir, device)
+            evaluate(
+                actor_critic,
+                ob_rms,
+                args.env_name,
+                args.seed,
+                args.num_processes,
+                eval_log_dir,
+                device,
+            )
 
 
 if __name__ == "__main__":
