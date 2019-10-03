38fed85f3ce998005db8a0a07d107a65dd740d44
Brendan Whitaker <whitaker.213@osu.edu>
2019-10-03 16:32:26 +0000

    Begun adding docstrings to all functions.

--- bees/agent.py
+++ bees/agent.py
@@ -10,7 +10,44 @@ from utils import convert_obs_to_tuple
 
 
 class Agent:
-    """ An agent with position and health attributes. """
+    """
+    An agent with position and health attributes.
+
+    Parameters
+    ----------
+    sight_len : ``int``.
+        How far an agent can see in each cardinal direction.
+    num_obj_types : ``int``.
+        The number of distinct entity classes in the environment. Note that
+        we currently have only two (agents, food).
+    consts : ``Dict[str, Any]``.
+        Dictionary of various constants.
+    n_layers : ``int``.
+        Number of layers in the reward network.
+    hidden_dim : ``int``.
+        Hidden dimension of the reward network.
+    num_actions : ``int``.
+        The number of actions with which the input dimension is computed.
+    pos : ``Tuple[int, int]``, optional.
+        Current grid position of the agent.
+    initial_health : ``float``, optional.
+        Agent's health bar value at creation time.
+    reward_weights : ``List[np.ndarray]``, optional.
+        Weights of the agent's reward network.
+    reward_biases : ``List[np.ndarray]``, optional.
+        Biases of the agent's reward network.
+    reward_weight_mean : ``float``, optional.
+        Mean for weight initialization distribution.
+    reward_weight_stddev : ``float``, optional.
+        Standard deviation for weight initialization distribution.
+    mating_cooldown_len : ``int``, optional.
+        How long agent must wait in between mate actions.
+    """
+
+    # TODO: ``num_actions`` may be broken since actions are now multi-binary.
+    # TODO: Create a PretrainedAgent class which allows passing of the initial
+    #       health, reward weights and biases, and other things so that we can
+    #       make all arguments required in the init function.
 
     # pylint: disable=too-many-arguments
     def __init__(
@@ -44,7 +81,6 @@ class Agent:
         self.reward_weight_stddev = reward_weight_stddev
 
         # How many timesteps until they can mate again.
-        # MOD: Agents can no longer mate as soon as they are born.
         self.mating_cooldown = mating_cooldown_len
         self.mating_cooldown_len = mating_cooldown_len
 
@@ -72,7 +108,14 @@ class Agent:
             self.reward_biases = copy.deepcopy(reward_biases)
 
     def get_action(self) -> Tuple[int, int, int]:
-        """ Uses the policy to choose an action based on the observation. """
+        """
+        Uses the policy to choose an action based on the observation.
+
+        Returns
+        -------
+        action : ``Tuple[int, int, int]``.
+            Randomly generated action for dummy training runs.
+        """
         return self.policy.get_action(self.observation, self.health)
 
     def initialize_reward_weights(self) -> None:
@@ -104,7 +147,27 @@ class Agent:
             self.reward_biases.append(np.zeros((output_dim,)))
 
     def compute_reward(self, prev_health: float, action: Tuple[int, int, int]) -> float:
-        """ Computes agent reward given health value before consumption. """
+        """
+        Computes agent reward given health value before consumption.
+
+        Parameters
+        ----------
+        prev_health : ``float``.
+            Health on previous timestep.
+        action : ``Tuple[int, int, int]``.
+            The current chosen action.
+
+        Updates
+        -------
+        self.total_reward : ``float``.
+            The total reward summed over the agent's lifetime.
+
+        Returns
+        -------
+        scalar_reward : ``float``.
+            The reward computed via the reward network from the previous
+            health, action, and observation of the agent.
+        """
 
         flat_obs = np.array(self.observation).flatten()
         flat_action = self.get_flat_action(action)
@@ -126,22 +189,52 @@ class Agent:
         """
         Computes a binary vector (two concatentated one-hot vectors) which
         represents the action.
+
+        Parameters
+        ----------
+        action : ``Tuple[int, int, int]``.
+            The action as a tuple of integers representing the move, eat, and
+            mating subactions, respectively.
+
+        Returns
+        -------
+        action_array : ``np.ndarray``.
+            The action as an array of shape ``(num_actions,)`` with three
+            nonzero values (k-hot where k is the number of action types).
         """
+        # TODO: seems broken, we do not treat the mating action.
 
         action_vec = [0.0 for _ in range(self.num_actions)]
         action_vec[action[0]] = 1.0
         # HARDCODE
         # The 5 here represents the length of the move action space.
         action_vec[5 + action[1]] = 1.0
-        return np.array(action_vec)
+        action_array = np.array(action_vec)
+        return action_array
 
     def reset(self) -> Tuple[Tuple[Tuple[int, ...], ...], ...]:
-        """ Reset the agent. """
+        """
+        Reset the agent.
+
+        Returns
+        -------
+        self.observation : ``Tuple[Tuple[Tuple[int, ...], ...], ...]``.
+            The current agent observation.
+            Shape: ``(obs_len, obs_len, num_obj_types)``.
+        """
         self.health = self.initial_health
         self.total_reward = 0.0
         return self.observation
 
     def __repr__(self) -> str:
+        """
+        Returns one line of statistics for the agent.
+
+        Returns
+        -------
+        output : ``str``.
+            Health and total reward of the agent at current timestep.
+        """
         output = "Health: %f, " % self.health
         output += "Total reward: %f\n" % self.total_reward
         return output
