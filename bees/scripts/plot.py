d30eb91a3914a25b17f6e71251e091a3d1cb94cd
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-12-18 14:10:52 +0000

    Finished a prototype of 'scripts/plot.py'.
    Tested with a copy of 'plotplotplot' repo.

--- bees/scripts/plot.py
+++ bees/scripts/plot.py
@@ -1,14 +1,22 @@
 """ Plot data from environment log. """
-import json
-from typing import List
+import pprint
+import argparse
+from typing import List, Dict, Any
+
+import pandas as pd
+
+from plotplotplot.draw import graph
 
 
-def main() -> None:
-    with open(log_path, "r") as log_file:
-        steps = [json.loads(line.strip()) for line in log_file.readlines()]
+# pylint: disable=too-many-locals
+def main(args: argparse.Namespace) -> None:
+    """ Plot rewards from a log. """
+    with open(args.log_path, "r") as log_file:
+        steps: List[Dict[str, Any]] = [
+            eval(line.strip()) for line in log_file.readlines()
+        ]
     agent_data: Dict[int, Dict[str, List[Any]]] = {}
-    for i, step in steps.items():
-        iteration = step["iteration"]
+    for step in steps:
         agents: Dict[int, Dict[str, Any]] = step["agents"]
 
         # For each agent alive at this timestep.
@@ -29,10 +37,36 @@ def main() -> None:
                         value_list.append(agent_info[field])
                         agent_data[agent_id][field] = value_list
 
+    # Map ``agent_id`` to lists of last rewards.
     reward_key = "last_reward"
-    agent_reward_data: Dict[int, List[float]] = {}
-    # for agent_id, value_list_dict in agent_data.it
+    reward_log_map: Dict[int, List[float]] = {}
+    max_length = 0
+    for agent_id, value_list_dict in agent_data.items():
+        max_length = max(max_length, len(value_list_dict[reward_key]))
+        reward_log_map[agent_id] = value_list_dict[reward_key]
+
+    # Add padding to each list so they all have length ``max_length``.
+    for agent_id in reward_log_map:
+        reward_log = reward_log_map[agent_id]
+        if len(reward_log) < max_length:
+            reward_log.extend([float("nan")] * (max_length - len(reward_log)))
+            reward_log_map[agent_id] = reward_log
+
+    reward_df = pd.DataFrame.from_dict(reward_log_map)
+    save_path = os.path.join(os.path.dirname(args.log_path), 'reward_log.svg')
+    graph(
+        dfs=[reward_df],
+        ylabels=["rewards"],
+        filename="reward_log",
+        column_counts=[len(reward_log_map)],
+        phase="",
+        save_path=save_path,
+    )
+    print(reward_df)
 
 
 if __name__ == "__main__":
-    main()
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument("--log-path", required=True, type=str, help="Path to log.")
+    ARGS = PARSER.parse_args()
+    main(ARGS)
