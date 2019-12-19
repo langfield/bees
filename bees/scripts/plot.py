bd64d1e112ac0e48e56d8875ee07596881d044f2
Brendan <whitaker.213@osu.edu>
2019-12-19 12:04:32 -0500

    Numba and mp.

--- bees/scripts/plot.py
+++ bees/scripts/plot.py
@@ -1,13 +1,17 @@
 """ Plot data from environment log. """
 import os
+import time
 import argparse
+import multiprocessing as mp
 from typing import List, Dict, Any
 
 import pandas as pd
+from numba import jit
 
 from plotplotplot.draw import graph
 
 
+@jit(nopython=True)
 def parse_agent_data(steps: List[Dict[str, Any]]) -> Dict[int, Dict[str, List[Any]]]:
     """ Parse log data to be indexed by agent instead of by step. """
 
@@ -36,6 +40,7 @@ def parse_agent_data(steps: List[Dict[str, Any]]) -> Dict[int, Dict[str, List[An
     return agent_data
 
 
+@jit
 def get_rewards(agent_data: Dict[int, Dict[str, List[Any]]]) -> pd.DataFrame:
     """ Parses ``agent_data`` into a DataFrame with rewards for each agent. """
 
@@ -57,35 +62,65 @@ def get_rewards(agent_data: Dict[int, Dict[str, List[Any]]]) -> pd.DataFrame:
     reward_df = pd.DataFrame.from_dict(reward_log_map)
     return reward_df
 
+
 def get_child_count_map(agent_data: Dict[int, Dict[str, List[Any]]]) -> Dict[int, int]:
     """ Gets histogram-like data for number of children per agent. """
 
-    children_per_agent = {agent_id: agent_data[agent_id]['num_children'][-1] for agent_id in agent_data.keys()}
+    children_per_agent = {
+        agent_id: agent_data[agent_id]["num_children"][-1]
+        for agent_id in agent_data.keys()
+    }
     children_values = list(set(children_per_agent.values()))
     num_children = {}
     for children_value in children_values:
-        num_children[children_value] = len([agent_id for agent_id in children_per_agent if children_per_agent[agent_id] == children_value])
+        num_children[children_value] = len(
+            [
+                agent_id
+                for agent_id in children_per_agent
+                if children_per_agent[agent_id] == children_value
+            ]
+        )
 
     return num_children
+
 
 # pylint: disable=too-many-locals
 def main(args: argparse.Namespace) -> None:
     """ Plot rewards from a log. """
 
-    # Read and parse log.
+    def preprocess_line(line: str) -> str:
+        """ Strip and eval. """
+        return eval(line.strip())
+
+    @jit(nopython=True)
+    def readlines(log_file) -> List[str]:
+        """ Construct steps. """
+        steps: List[Dict[str, Any]] = []
+        for line in log_file:
+            steps.append(eval(line.strip()))
+        return steps
+
+    t_0 = time.time()
     with open(args.log_path, "r") as log_file:
-        steps: List[Dict[str, Any]] = [
-            eval(line.strip()) for line in log_file.readlines()
-        ]
+        steps = readlines(log_file)
+
+    print("Jit read: %fs" % (time.time - t_0))
+
+    # Read and parse log.
+    t_0 = time.time()
+    pool = mp.Pool()
+    with open(args.log_path, "r") as log_file:
+        steps: List[Dict[str, Any]] = pool.map(preprocess_line, log_file.readlines())
+    print("MP read: %fs" % (time.time - t_0))
     agent_data = parse_agent_data(steps)
 
     # Get individual metrics from parsed data.
     reward_df = get_rewards(agent_data)
-    child_count_map = get_child_count_map(agent_data)
+    # child_count_map = get_child_count_map(agent_data)
 
     # Plot or write out individual metrics.
     save_dir = os.path.dirname(args.log_path)
-    plot_path = os.path.join(save_dir, 'reward_log.svg')
+    plot_path = os.path.join(save_dir, "reward_log.svg")
     graph(
         dfs=[reward_df],
         y_labels=["rewards"],
@@ -93,9 +128,12 @@ def main(args: argparse.Namespace) -> None:
         save_path=plot_path,
         settings_path=args.settings_path,
     )
+
+    """
     children_path = os.path.join(save_dir, "num_children.txt")
     with open(children_path, "w") as children_file:
         children_file.write(str(child_count_map))
+    """
 
 
 if __name__ == "__main__":
