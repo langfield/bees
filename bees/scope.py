7adf771e94656245a98afb579519bdafcca6a26b
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2019-12-24 00:22:04 +0000

    Completed single agent linear reward trial. Added scope.py.

---
+++ bees/scope.py
@@ -0,0 +1,10 @@
+""" Analyzes the reward network of an agent. """
+
+def main():
+    # Read in env.pkl.
+    # Select agent to analyze.
+    # Iterate over action space and call ``agent.compute_reward()``.
+    # Plot the results.
+    raise NotImplementedError
+if __name__ == "__main__":
+    main()
