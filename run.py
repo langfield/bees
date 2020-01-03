b21c7d950bbced6bb285fdce60b843b6e3db1005
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-01-03 18:14:37 +0000

    Converted to package, now runs with run.py.

---
+++ run.py
@@ -0,0 +1,26 @@
+import argparse
+
+from bees.trainer import train
+
+
+def main() -> None:
+    """ Main function to run bees environment. """
+
+    # Get and validate args.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--load-from", default="", help="Saved directory to load from.")
+    parser.add_argument("--settings", default="", help="Settings file to use.")
+    parser.add_argument(
+        "--save-root",
+        default="./models/",
+        help="directory to save agent logs (default: ./models/)",
+    )
+
+    args = parser.parse_args()
+
+    # Run training.
+    train(args)
+
+
+if __name__ == "__main__":
+    main()
