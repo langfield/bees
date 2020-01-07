8aa881dcf422823e7d703f550cc22ee5613910ec
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-01-07 15:38:06 +0000

    Removed gail.py and removed unused variable in ppo.py.

--- bees/a2c_ppo_acktr/algo/ppo.py
+++ bees/a2c_ppo_acktr/algo/ppo.py
@@ -41,7 +41,7 @@ class PPO:
         action_loss_epoch = 0
         dist_entropy_epoch = 0
 
-        for e in range(self.ppo_epoch):
+        for _ in range(self.ppo_epoch):
             if self.actor_critic.is_recurrent:
                 data_generator = rollouts.recurrent_generator(
                     advantages, self.num_mini_batch
