c4b614a0a38c99ce5464e7668b962dc166f956b2
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-01-15 14:39:36 +0000

    Added computation of task score for zero-to-food.
    Added food_score to Metrics and computation of food_score to analysis.py.
    Changed 'env._get_optimal_action_dists()' to 'env.get_optimal_action_dists()'.
    Added call to 'update_food_scores()' in trainer.py.

--- bees/analysis.py
+++ bees/analysis.py
@@ -2,11 +2,13 @@
 from typing import Dict, Any, Tuple
 import copy
 
+import numpy as np
 import torch
 import torch.nn.functional as F
 
 from bees.env import Env
 from bees.config import Config
+from bees.utils import flat_action_to_tuple
 
 # pylint: disable=too-few-public-methods
 
@@ -28,6 +30,10 @@ class Metrics:
         self.dist_entropy: float = float("inf")
         self.total_loss: float = float("inf")
         self.initial_policy_score = float("inf")
+
+        # Task specific score (for now, zero-to-food).
+        self.food_scores: Dict[int, float] = {}
+        self.food_score: float = float("inf")
 
 
 def aggregate_loss(env: Env, losses: Dict[int, float]) -> float:
@@ -168,3 +174,92 @@ def update_losses(
     new_metrics.total_loss = aggregate_loss(env, new_metrics.total_losses)
 
     return new_metrics
+
+
+def update_food_scores(env: Env, metrics: Metrics) -> Metrics:
+    """
+    Computes update food scores from agent reward networks.
+
+    Parameters
+    ----------
+    env : ``Env``.
+        Training environment.
+    metrics : ``Metrics``.
+        The state of the training analysis metrics. Not mutated.
+
+    Returns
+    -------
+    new_metrics : ``Metrics``.
+        Updated version of ``metrics``. This is not the same object.
+    """
+
+    new_metrics = copy.deepcopy(metrics)
+    previous_agent_ids = set(new_metrics.food_scores.keys())
+
+    # Compute individual food scores for any new agents.
+    # TODO: Fix this inefficiency.
+    # Calling ``env.get_optimal_action_dists()`` is pretty inefficient, because this
+    # function will compute the optimal action distribution for each agent, even though
+    # we only need it for newly born agents.
+    optimal_action_dists = env.get_optimal_action_dists()
+    target_dist = get_food_target_dist(env)
+    for agent_id in env.agents:
+        if agent_id not in new_metrics.food_scores:
+            # TODO: Compute KL-divergence with non-greedy optimal distribution.
+            # Currently, since the KL-divergence isn't defined when q(x) = 0 and
+            # p(x) != 0 for some x, we can't use the argmax optimal distribution. But
+            # the two computations (policy score loss and food score loss) should be
+            # decoupled, so that we can use the non-greedy version here and the greedy
+            # version for the policy score loss, if we choose.
+            new_metrics.food_scores[agent_id] = float(
+                F.kl_div(torch.log(optimal_action_dists[agent_id]), target_dist)
+            )
+
+    # Remove food scores for any agents that have died.
+    intermediate_agent_ids = new_metrics.food_scores.keys()
+    for agent_id in intermediate_agent_ids:
+        if agent_id not in env.agents:
+            del new_metrics.food_scores[agent_id]
+
+    # Recompute average food scores, if relevant agents have changed.
+    current_agent_ids = set(new_metrics.food_scores.keys())
+    if current_agent_ids != previous_agent_ids:
+        new_metrics.food_score = np.mean(list(new_metrics.food_scores.values()))
+
+    return new_metrics
+
+
+def get_food_target_dist(env: Env) -> torch.Tensor:
+    """
+    Computes target action distribution for the zero-to-food task. This distribution is
+    uniform over all actions which involve the 'eat' subaction, and zero everywhere
+    else.
+
+    Parameters
+    ----------
+    env : ``Env``.
+        Training environment.
+
+    Returns
+    -------
+    target_dist : ``torch.Tensor``.
+        Optimal action distribution for the zero-to-food task.
+    """
+
+    # HARDCODE
+    EAT_INDEX = 1
+    target_dist = torch.zeros((env.num_actions,))
+    num_eat_actions = 0
+
+    # Count actions that involve eating, and set values in ``target_dist``.
+    for flat_action in range(env.num_actions):
+        action_tuple = flat_action_to_tuple(flat_action, env.subaction_sizes)
+
+        if action_tuple[EAT_INDEX] == 1:
+            target_dist[flat_action] = 1.0
+            num_eat_actions += 1
+
+    # Normalize values in ``target_dist``.
+    target_dist /= num_eat_actions
+
+    return target_dist
