6138194e37fbc556eff1d4d192ea6cb4dbd14a65
Brendan Whitaker <whitaker.213@osu.edu>
2020-01-20 19:35:59 +0000

    Added queues to 'worker.py'. Removed a lot of boilerplate.
    Changed some variable names in 'worker.py'.

--- bees/worker.py
+++ bees/worker.py
@@ -1,7 +1,10 @@
 """ Distributed training function for a single agent worker. """
 import collections
+from typing import Dict, Any
 
 import torch
+import torch.nn.functional as F
+import torch.multiprocessing as mp
 
 from bees.rl import utils
 from bees.rl.storage import RolloutStorage
@@ -12,6 +15,39 @@ from bees.config import Config
 
 # pylint: disable=duplicate-code
 
+STOP_FLAG = 999
+
+
+def update_learning_rate(agent_id: int, agent: Algo, config: Config, env: Env) -> None:
+    """ Compute age and minimum agent lifetime. """
+    # TODO: Figure out how to share ``env`` in memory.
+    env_agent = env.agents[agent_id]
+    age = env_agent.age
+    min_agent_lifetime = 1.0 / config.aging_rate
+
+    # Decrease learning rate linearly.
+    learning_rate = utils.update_linear_schedule(
+        agent.optimizer,
+        age,
+        min_agent_lifetime,
+        agent.optimizer.lr if config.algo == "acktr" else config.lr,
+        config.min_lr,
+    )
+
+    agent.lr = learning_rate
+
+
+def get_policy_score(action_dist: torch.Tensor, info: Dict[str, Any]) -> float:
+    """ Compute the policy score given current and optimal distributions. """
+    optimal_action_dist = info["optimal_action_dist"]
+    action_dist = action_dist.cpu()
+
+    timestep_score = float(
+        F.kl_div(torch.log(action_dist), optimal_action_dist, reduction="sum")
+    )
+
+    return timestep_score
+
 
 def single_agent_loop(
     agent_id: int,
@@ -20,100 +56,102 @@ def single_agent_loop(
     config: Config,
     env: Env,
     num_updates: int,
-):
+    initial_step: int,
+    env_queue: mp.Queue,
+    action_queue: mp.Queue,
+    action_dist_queue: mp.Queue,
+    loss_queue: mp.Queue,
+) -> None:
     """ Training loop for a single agent worker. """
 
-    agent_episode_rewards = collections.deque(maxlen=10)
+    step = initial_step
+    agent_episode_rewards: collections.deque = collections.deque(maxlen=10)
 
-    for j in range(num_updates):
+    # TODO: Compute/pass this!!!
+    num_updates_condition = float("inf")
 
-        if config.use_linear_lr_decay:
+    while True:
 
-            # Compute age and minimum agent lifetime.
-            env_agent = env.agents[agent_id]
-            age = env_agent.age
-            min_agent_lifetime = 1.0 / config.aging_rate
+        # Only when trainer would make an update/backward pass.
+        if step % num_updates_condition == 0 and config.use_linear_decay:
+            update_learning_rate(agent_id, agent, config, env)
 
-            # Decrease learning rate linearly.
-            learning_rate = utils.update_linear_schedule(
-                agent.optimizer,
-                age,
-                min_agent_lifetime,
-                agent.optimizer.lr if config.algo == "acktr" else config.lr,
-                config.min_lr,
+        # TODO: Consider moving this block and the above to the bottom so that
+        # ``env_queue.get()`` is the first statement in the loop.
+        # This would require running it once at the top on agent's first iteration
+        # when step == initial_step, which is gross.
+        with torch.no_grad():
+            act_returns = agent.actor_critic.act(
+                rollouts.obs[step],
+                rollouts.recurrent_hidden_states[step],
+                rollouts.masks[step],
             )
 
-            agent.lr = learning_rate
+            # Get integer action to pass to ``env.step()``.
+            env_action: int = int(act_returns[1][0])
 
-        for step in range(config.num_steps):
+            # These are all CUDA tensors (on device).
+            value: torch.Tensor = act_returns[0]
+            action: torch.Tensor = act_returns[1]
+            action_log_prob: torch.Tensor = act_returns[2]
+            recurrent_hidden_states: torch.Tensor = act_returns[3]
+            action_dist: torch.Tensor = act_returns[4]
 
-            # Sample actions.
-            with torch.no_grad():
-                ac_tuple = agent.actor_critic.act(
-                    rollouts.obs[step],
-                    rollouts.recurrent_hidden_states[step],
-                    rollouts.masks[step],
-                )
+        # TODO: Send ``env_action: int`` back to leader to execute step.
+        action_queue.put(env_action)
 
-                value: torch.Tensor = ac_tuple[0]
-                env_action: int = int(ac_tuple[1][0])
-                action: torch.Tensor = ac_tuple[1]
-                action_log_prob: torch.Tensor = ac_tuple[2]
-                recurrent_hidden_states: torch.Tensor = ac_tuple[3]
-                agent_action_dist: torch.Tensor = ac_tuple[4]
+        # Execute environment step.
+        # TODO: Grab step index and output from leader (no tensors included).
+        step, ob, reward, done, info = env_queue.get()
 
-            # TODO: Send ``env_action`` back to leader to execute step.
+        # Update the policy score.
+        # TODO: Send ``action_dist`` back to leader to update_policy_score.
+        # TODO: This should be done every k steps on workers instead of leader.
+        # Then we just send the floats back to leader, which is cheaper.
+        timestep_score = get_policy_score(action_dist, info)
+        action_dist_queue.put(timestep_score)
 
-            # Execute environment step.
-            # TODO: Grab step output from leader.
-            action_dict = {}
-            obs, rewards, dones, infos = env.step(action_dict)
+        # Rollout stacking.
+        # TODO: Figure out this condition.
+        if agent_id in agents:
+            if "episode" in info.keys():
+                agent_episode_rewards[agent_id].append(info["episode"]["r"])
 
-            # Update the policy score.
-            # TODO: Send agent_action_dist back to leader to update_policy_score.
+            # If done then clean the history of observations.
+            if done:
+                masks = torch.FloatTensor([[0.0]])
+            else:
+                masks = torch.FloatTensor([[1.0]])
+            if "bad_transition" in info.keys():
+                bad_masks = torch.FloatTensor([[0.0]])
+            else:
+                bad_masks = torch.FloatTensor([[1.0]])
 
-            # Agent creation and termination, rollout stacking.
-            agent_obs = obs[agent_id]
-            agent_reward = rewards[agent_id]
-            agent_done = dones[agent_id]
-            agent_info = infos[agent_id]
+            # If done then remove from environment.
+            # TODO: Tell leader to remove from environment.
+            if done:
+                action_queue.put(STOP_FLAG)
+                pass
 
-            if agent_id in agents:
-                if "episode" in agent_info.keys():
-                    agent_episode_rewards[agent_id].append(agent_info["episode"]["r"])
+            # Shape correction and casting.
+            # TODO: Change names so everything is statically-typed.
+            observation = torch.FloatTensor([ob])
+            reward = torch.FloatTensor([reward])
 
-                # If done then clean the history of observations.
-                if agent_done:
-                    masks = torch.FloatTensor([[0.0]])
-                else:
-                    masks = torch.FloatTensor([[1.0]])
-                if "bad_transition" in agent_info.keys():
-                    bad_masks = torch.FloatTensor([[0.0]])
-                else:
-                    bad_masks = torch.FloatTensor([[1.0]])
+            # Add to rollouts.
+            rollouts.insert(
+                observation,
+                recurrent_hidden_states,
+                action,
+                action_log_prob,
+                value,
+                reward,
+                masks,
+                bad_masks,
+            )
 
-                # If done then remove from environment.
-                # TODO: Tell leader to remove from environment.
-                if agent_done:
-                    pass
-
-                # Shape correction and casting.
-                observation = torch.FloatTensor([agent_obs])
-                reward = torch.FloatTensor([agent_reward])
-
-                # Add to rollouts.
-                rollouts.insert(
-                    observation,
-                    recurrent_hidden_states,
-                    action,
-                    action_log_prob,
-                    value,
-                    reward,
-                    masks,
-                    bad_masks,
-                )
-
-        if env.agent.age > 0:
+        # Only when trainer would make an update/backward pass.
+        if step % num_updates_condition == 0 and env.agent.age > 0:
 
             with torch.no_grad():
                 next_value = agent.actor_critic.get_value(
@@ -134,17 +172,5 @@ def single_agent_loop(
             value_loss, action_loss, dist_entropy = agent.update(rollouts)
             rollouts.after_update()
 
-        # +++++CONVERT+++++
-        # TODO: Send losses back to leader for ``update_losses()``.
-        # TODO: Break if done.
-        """
-        metrics = update_losses(
-            env=env,
-            config=config,
-            losses=(value_losses, action_losses, dist_entropies),
-            metrics=metrics,
-        )
-        if env_done:
-            break
-        """
-        # +++++CONVERT+++++
+            # TODO: Send losses back to leader for ``update_losses()``.
+            loss_queue.put((value_loss, action_loss, dist_entropy))
