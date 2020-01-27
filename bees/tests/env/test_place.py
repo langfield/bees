707f1bef49509ad1358d514b918457772c2bc5cb
Brendan Whitaker <whitaker.213@osu.edu>
2020-01-26 17:46:12 -0700

    Added many tests for 'Env'.
    Shorted filenames in 'bees/tests/env/'.
    Added tests in 'test_obj_exists.py'.
    TODO: Consider making grid toroidal.
    TODO: Change all Error tests to 'pytest.raises(<Error_Type>)'.

---
+++ bees/tests/env/test_place.py
@@ -0,0 +1,94 @@
+""" Test that ``Env.reset()`` works correctly. """
+from typing import Tuple, Set
+
+import pytest
+import hypothesis.strategies as st
+from hypothesis import given
+
+from bees.env import Env
+from bees.tests import strategies
+
+# pylint: disable=no-value-for-parameter, protected-access
+
+# TODO: Convert to ``@given(st.data())``.
+@given(strategies.grid_positions())
+def test_env_place_without_id(place_args: Tuple[Env, Tuple[int, int]]) -> None:
+    """ Tests placement of an object without id (such as food). """
+    env, pos = place_args
+    env.reset()
+    obj_type_id = env.obj_type_ids["food"]
+    if not env._obj_exists(obj_type_id, pos):
+        env._place(obj_type_id, pos)
+
+    grid_idx = pos + (obj_type_id,)
+    assert env.grid[grid_idx] == 1
+
+
+@given(strategies.grid_positions())
+def test_env_place_with_id(place_args: Tuple[Env, Tuple[int, int]]) -> None:
+    """ Tests placement of an object with id (such as an agent). """
+    env, pos = place_args
+    x, y = pos
+    env.reset()
+    obj_type_id = env.obj_type_ids["agent"]
+    if not env._obj_exists(obj_type_id, pos):
+        agent_id = env._new_agent_id()
+        env._place(obj_type_id, pos, agent_id)
+    grid_idx = pos + (obj_type_id,)
+    assert env.grid[grid_idx] == 1
+
+
+@given(strategies.grid_positions())
+def test_env_place_with_id_requires_id(place_args: Tuple[Env, Tuple[int, int]]) -> None:
+    """ Tests that env gets angry if you don't pass an object id for het object. """
+    env, pos = place_args
+    x, y = pos
+    env.reset()
+    obj_type_id = env.obj_type_ids["agent"]
+    if not env._obj_exists(obj_type_id, pos):
+        agent_id = env._new_agent_id()
+        with pytest.raises(TypeError):
+            env._place(obj_type_id, pos)
+
+
+@given(strategies.grid_positions())
+def test_env_place_no_double_place(place_args: Tuple[Env, Tuple[int, int]]) -> None:
+    """ Tests that env gets angry if you try to double up het objs with same id. """
+    env, pos = place_args
+    x, y = pos
+    env.reset()
+    obj_type_id = env.obj_type_ids["agent"]
+    if not env._obj_exists(obj_type_id, pos):
+        agent_id = env._new_agent_id()
+        env._place(obj_type_id, pos, agent_id)
+        with pytest.raises(ValueError):
+            env._place(obj_type_id, pos, agent_id)
+
+
+@given(st.data())
+def test_env_place_no_double_place_homo(data: st.DataObject) -> None:
+    """ Tests that env gets angry if you try to double up homo objs. """
+    env = data.draw(strategies.envs())
+    pos = data.draw(strategies.positions(env=env))
+    x, y = pos
+    env.reset()
+    homo_obj_type_ids = set(env.obj_type_ids.values()) - env.heterogeneous_obj_type_ids
+    obj_type_id = data.draw(st.sampled_from(list(homo_obj_type_ids)))
+
+    if not env._obj_exists(obj_type_id, pos):
+        env._place(obj_type_id, pos)
+        with pytest.raises(ValueError):
+            env._place(obj_type_id, pos)
+
+
+@given(strategies.grid_positions())
+def test_env_place_with_id_in_id_map(place_args: Tuple[Env, Tuple[int, int]]) -> None:
+    """ Tests placement of an object with id (such as an agent). """
+    env, pos = place_args
+    x, y = pos
+    env.reset()
+    obj_type_id = env.obj_type_ids["agent"]
+    if not env._obj_exists(obj_type_id, pos):
+        agent_id = env._new_agent_id()
+        env._place(obj_type_id, pos, agent_id)
+        assert agent_id in env.id_map[x][y][obj_type_id]
---
+++ bees/tests/env/test_plant.py
@@ -0,0 +1,34 @@
+import hypothesis.strategies as st
+from hypothesis import given
+
+from bees.env import Env
+from bees.tests import strategies as bst
+# pylint: disable=no-value-for-parameter
+
+
+# TODO: Can we even test anything else here (density calculation)?
+@given(st.data())
+def test_env_plant_doesnt_remove_food(data: st.DataObject) -> None:
+    env: Env = data.draw(bst.envs())
+    old_num_foods = env.num_foods
+    env._plant()
+    assert env.num_foods >= old_num_foods
+
+
+@given(st.data())
+def test_env_plant_does_nothing_when_zero_prob(data: st.DataObject) -> None:
+    env: Env = data.draw(bst.envs())
+    env.adaptive_food = False
+    env.food_regen_prob = 0.0
+    old_num_foods = env.num_foods
+    env._plant()
+    assert env.num_foods == old_num_foods
+
+
+@given(st.data())
+def test_env_plant_fills_grid_when_one_prob(data: st.DataObject) -> None:
+    env: Env = data.draw(bst.envs())
+    env.adaptive_food = False
+    env.food_regen_prob = 1.0
+    env._plant()
+    assert env.num_foods == env.width * env.height
