45200c548548c7788f30172c03f6e34f69be034d
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-02-12 14:03:22 +0000

    Finished test_get_obs.py.
    Fixed swapped height and width axes in observation from env._get_obs().
    Added a test to ensure that observations are binary valued.
    Removed unused variables ALPHA and DEBUG from trainer.py.

--- bees/tests/env/test_get_obs.py
+++ bees/tests/env/test_get_obs.py
@@ -18,6 +18,17 @@ def test_get_obs_has_correct_shape(data: st.DataObject) -> None:
 
 
 @given(st.data())
+def test_get_obs_has_no_out_of_range_elements(data: st.DataObject) -> None:
+    """ Make sure that a returned observation only contains 0 or 1. """
+    env = data.draw(bst.envs())
+    env.reset()
+    pos = data.draw(bst.positions(env=env))
+    ob = env._get_obs(pos)
+    for elem in np.nditer(ob):
+        assert elem == 0.0 or elem == 1.0
+
+
+@given(st.data())
 def test_get_obs_has_correct_objects(data: st.DataObject) -> None:
     """ Make sure that a returned observation is accurate w.r.t. ``env.grid``. """
     env = data.draw(bst.envs())
@@ -27,14 +38,9 @@ def test_get_obs_has_correct_objects(data: st.DataObject) -> None:
     for i in range(ob.shape[1]):
         for j in range(ob.shape[2]):
             ob_pos = (pos[0] + i - env.sight_len, pos[1] + j - env.sight_len)
-            print("=========================================")
             if 0 <= ob_pos[0] < env.width and 0 <= ob_pos[1] < env.height:
-                print(f"Shape of 'ob': {ob.shape}")
-                print(f"Shape of 'grid': {env.grid.shape}")
                 ob_square = ob[:, i, j]
                 env_square = env.grid[ob_pos]
-                print(f"Shape of 'ob_square': {ob_square.shape}")
-                print(f"Shape of 'env_square': {env_square.shape}")
                 assert np.all(ob_square == env_square)
             else:
                 assert np.all(ob[:, i, j] == np.zeros((env.num_obj_types,)))
