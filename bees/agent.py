Fixed observation feature computation and made tabular rewards optional.

bees/agent.py
@@ -80,7 +80,10 @@ class Agent(Config):
         self.obs_shape = (self.num_obj_types, self.obs_width, self.obs_width)
         self.observation: np.ndarray = np.zeros(self.obs_shape)
 
-        self.input_dim = self.set_reward_network_tabular_input_dim()
+        if config.tabular:
+            self.input_dim = self.set_reward_network_tabular_input_dim()
+        else:
+            self.input_dim = self.set_reward_network_input_dim()
 
         # Initialize/set reward weights and biases.
         if reward_weights is None:

bees/analysis.py
@@ -251,7 +251,9 @@ def get_optimal_action_dists(
     for agent_id, agent in agents.items():
         action_rewards = torch.zeros((num_actions,))
         for action in range(num_actions):
-            action_rewards[action] = agent.compute_tabular_reward()
+
+            # Note that this isn't currently compatible with tabular rewards.
+            action_rewards[action] = agent.compute_reward()
 
         # Flatten action_rewards, perform softmax, and return to original shape.
         # This is because torch.nn.functional.softmax only computes softmax along

bees/utils.py
@@ -218,7 +218,7 @@ def get_observation_features(ob: Array[int]) -> Array[int, -1]:
         for j, cell in enumerate(row):
             dx = i - maxradii[0]
             dy = j - maxradii[1]
-            norm = math.sqrt(dx ** 2 + dy ** 2)
+            norm = 1 / (math.sqrt(dx ** 2 + dy ** 2) + 1)
             max_density += norm
             if cell == 1:
                 agent_distance_density += norm
@@ -230,7 +230,7 @@ def get_observation_features(ob: Array[int]) -> Array[int, -1]:
             if cell == 1:
                 dx = i - maxradii[0]
                 dy = j - maxradii[1]
-                norm = math.sqrt(dx ** 2 + dy ** 2)
+                norm = 1 / (math.sqrt(dx ** 2 + dy ** 2) + 1)
                 food_distance_density += norm
 
     unnormed_densities = np.array([agent_distance_density, food_distance_density])